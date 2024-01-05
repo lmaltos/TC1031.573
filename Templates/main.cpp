261a6c3d37f88a8e487f2b551efd60085f41600d
lmaltos <136147644+lmaltos@users.noreply.github.com>
2024-01-05 02:26:26 +0000

    Agrega doble template

--- Templates/main.cpp
+++ Templates/main.cpp
@@ -2,15 +2,16 @@
 #include <iostream>
 
 int main() {
-    myClass<int> x(99,100,4.5f),y(1,2,'x');
-    myClass<char> z('x','y','z');
-    myClass<myClass<int>> w(x,y,'z');
+    myClass<int,char> x(99,100,4.5f),y(1,2,'x');
+    myClass<char,std::string> z('x','y',"Hola mundo de los templates");
+    myClass<myClass<int,char>,char> w(x,y,'z');
     std::cout << "x.a = " << x.getA() << std::endl;
     std::cout << "x.b = " << x.getB() << std::endl;
     std::cout << "y.a = " << y.getA() << std::endl;
     std::cout << "y.b = " << y.getB() << std::endl;
     std::cout << "z.a = " << z.getA() << std::endl;
     std::cout << "z.b = " << z.getB() << std::endl;
+    std::cout << "z.c = " << z.getC() << std::endl;
     std::cout << "w.a.a = " << w.getA().getA() << std::endl;
     std::cout << "w.b.b = " << w.getB().getB() << std::endl;
     return 0;
--- Templates/myClass.h
+++ Templates/myClass.h
@@ -1,35 +1,36 @@
 #ifndef MYCLASS_H
 #define MYCLASS_H
 
-template <typename T>
+template <typename T, typename D>
 class myClass {
   private:
     T a;
     T b;
-    char c;
+    D c;
     float f;
   public:
     myClass(); // <- constructor default
-    myClass(T a, T b, char c); // constructor alternativo
+    myClass(T a, T b, D c); // constructor alternativo
     myClass(T a, T b, float f); // otro constructor altrnativo
     T getA() {return a;}
     T getB() {return b;}
+    D getC() {return c;}
 };
 
-template <typename T>
-myClass<T>::myClass(){
+template <typename T, typename D>
+myClass<T,D>::myClass(){
     
 }
 
-template <typename T>
-myClass<T>::myClass(T a, T _b, char _c){
+template <typename T, typename D>
+myClass<T,D>::myClass(T a, T _b, D _c){
     this->a = a;
     b = _b;
     c = _c;
 }
 
-template <typename D>
-myClass<D>::myClass(D a, D b, float f){
+template <typename D, typename E>
+myClass<D,E>::myClass(D a, D b, float f){
     this->a = a;
     this->b = b;
     this->f = f;
