9a1ccf4f8ce7b9dc48d7bdce0962239a52770a94
lmaltos <136147644+lmaltos@users.noreply.github.com>
2024-01-06 01:52:29 +0000

    Funciones recursivas

---
+++ Recursividad/funciones.cpp
@@ -0,0 +1,43 @@
+#include "funciones.h"
+
+/// @brief Numeros de fibonacci
+/// @param n numero buscado
+/// @return Regresa el numero fibonacci de la posicion n
+int fibonacci(int n) {
+    if (n < 3) {
+        return 1;
+    }
+    return fibonacci(n-1) + fibonacci(n-2);
+}
+
+/// @brief Calcular el factorial de un numero
+/// @param n numero a calcular el factorial
+/// @return el factorial de n, n!
+int factorial(int n) {
+    if (n < 2) {
+        return 1;
+    }
+    return n * factorial(n - 1);
+}
+
+int funcion91(int n) {
+    if (n > 100) {
+        return n - 10;
+    }
+    else {
+        return funcion91(funcion91(n + 11));
+    }
+}
+
+/// @brief Calcular el numero de iteraciones para llegar a 1
+/// @param n 
+/// @return 
+int tresNMasUno(int n) {
+    if (n % 2 == 0) {// n & 1
+        n = n / 2;
+    }
+    else {
+        n = 3 * n + 1;
+    }
+    return 1 + tresNMasUno(n);
+}
---
+++ Recursividad/funciones.h
@@ -0,0 +1,9 @@
+#ifndef FUNCIONES_H
+#define FUNCIONES_H
+
+int fibonacci(int);
+int factorial(int);
+int funcion91(int);
+int tresNMasUno(int);
+
+#endif
