3bd97819ddaabf836a782deef60e02368d93cde4
MALTOS <LUIS.MALTOS@pepsico.com>
2024-01-25 18:36:51 -0600

    Se agrega grafo con lista de adyacencia

--- Grafos/grafoLA.h
+++ Grafos/grafoLA.h
@@ -2,46 +2,18 @@
 #define GRAFOLA_H
 #include <string>
 
-class nodoAdyacente;
-class nodoLA;
 
-class grafoLA {
+class nodoAdyacente {
   private:
-    nodoLA *head;
-    bool dirigido = true;
-    nodoLA* addNodo(std::string);
+    std::string id;
+    nodoAdyacente *next;
   public:
-    grafoLA() {head = nullptr;}
-    nodoLA *buscarNodo(std::string);
-    void addAdyacencia(std::string,std::string);
-    bool sonAdyacentes(std::string,std::string);
+    nodoAdyacente(std::string);
+    nodoAdyacente(std::string, nodoAdyacente*);
+    std::string getId() {return id;}
+    nodoAdyacente* getNext() {return next;}
+    void setNext(nodoAdyacente* _next) {next = _next;}
 };
-
-nodoLA *grafoLA::buscarNodo(std::string id) {
-    nodoLA *p = head;
-    while (p != nullptr) {
-        if (p->getId() == id) {
-            return p;
-        }
-        p = p->getNext();
-    }
-}
-
-nodoLA* grafoLA::addNodo(std::string id) {
-
-}
-
-void grafoLA::addAdyacencia(std::string idA,std::string idB) {
-    nodoLA *A = buscarNodo(idA);
-    nodoLA *B = buscarNodo(idB);
-    A->addAdyacencia(idB);
-    if (dirigido)
-        B->addAdyacencia(idA);
-}
-
-bool grafoLA::sonAdyacentes(std::string idA,std::string idB){
-    return true; // ToDo
-}
 
 class nodoLA {
   private:
@@ -57,6 +29,74 @@ class nodoLA {
     void addAdyacencia(std::string);
     void delAdyacencia(std::string);
 };
+
+class grafoLA {
+  private:
+    nodoLA *head;
+    bool dirigido = true;
+    nodoLA* addNodo(std::string);
+  public:
+    grafoLA() {head = nullptr;}
+    nodoLA *buscarNodo(std::string);
+    void addAdyacencia(std::string,std::string);
+    bool sonAdyacentes(std::string,std::string);
+};
+
+nodoLA::nodoLA(std::string _id) {
+    id = _id;
+    head = nullptr;
+    next = nullptr;
+}
+
+nodoLA *grafoLA::buscarNodo(std::string id) {
+    nodoLA *p = head;
+    while (p != nullptr) {
+        if (p->getId() == id) {
+            return p;
+        }
+        p = p->getNext();
+    }
+    return addNodo(id);
+}
+
+nodoLA* grafoLA::addNodo(std::string id) {
+    nodoLA *p = head, *tail = nullptr;
+    while (p != nullptr) {
+        tail = p;
+        p = p->getNext();
+    }
+    p = new nodoLA(id);
+    if (tail != nullptr) {
+        tail->setNext(p);
+    }
+    else {
+        head = p;
+    }
+    return p;
+}
+
+void grafoLA::addAdyacencia(std::string idA,std::string idB) {
+    nodoLA *A = buscarNodo(idA);
+    nodoLA *B = buscarNodo(idB);
+    A->addAdyacencia(idB);
+    if (dirigido)
+        B->addAdyacencia(idA);
+}
+
+bool grafoLA::sonAdyacentes(std::string idA,std::string idB){
+    nodoLA *A = buscarNodo(idA);
+    return A->esAdyacente(idB);
+}
+
+nodoAdyacente::nodoAdyacente(std::string _id) {
+    id = _id;
+    next = nullptr;
+}
+
+nodoAdyacente::nodoAdyacente(std::string _id, nodoAdyacente* _next) {
+    id = _id;
+    next = _next;
+}
 
 bool nodoLA::esAdyacente(std::string idA) {
     nodoAdyacente *p = head;
@@ -89,28 +129,6 @@ void nodoLA::delAdyacencia(std::string idB) {
         q = p;
         p = p->getNext();
     }
-}
-
-class nodoAdyacente {
-  private:
-    std::string id;
-    nodoAdyacente *next;
-  public:
-    nodoAdyacente(std::string);
-    nodoAdyacente(std::string, nodoAdyacente*);
-    std::string getId() {return id;}
-    nodoAdyacente* getNext() {return next;}
-    void setNext(nodoAdyacente* _next) {next = _next;}
-};
-
-nodoAdyacente::nodoAdyacente(std::string _id) {
-    id = _id;
-    next = nullptr;
-}
-
-nodoAdyacente::nodoAdyacente(std::string _id, nodoAdyacente* _next) {
-    id = _id;
-    next = _next;
 }
 
 #endif
--- Grafos/main.cpp
+++ Grafos/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 #include "grafoMA.h"
 #include "grafoLA.h"
 
@@ -6,7 +7,22 @@ using namespace std;
 
 int main() {
     grafoLA G;
-    G.addAdyacencia("A","B"); // crear nodos A y B y las adyacencias A->B y B->A
+    G.addAdyacencia("A","B"); // crear nodos A y B y las adyacencias A->B y B->A    G.addArco('A','B');
+    G.addAdyacencia("A","C");
+    G.addAdyacencia("A","E");
+    G.addAdyacencia("B","D");
+    G.addAdyacencia("C","D");
+    G.addAdyacencia("D","E");
+    vector<string> ids = {"A","B","C","D","E"};
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 5; j++) {
+            if (i != j) {
+                if (G.sonAdyacentes(ids[i],ids[j])) {
+                    cout << ids[i] << " -> " << ids[j] << endl;
+                }
+            }
+        }    
+    }
     return 0;
 }
 
